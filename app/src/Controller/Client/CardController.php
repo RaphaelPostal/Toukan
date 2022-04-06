86231210760d8096b35313f339db6abcc575a014
fabien.sigrand <fabien_nancy@hotmail.fr>
2022-04-06 16:17:00 +0200

    creation card controller

---
+++ app/src/Controller/Client/CardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Client;
+
+use App\Entity\Establishment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/customer/establishment')]
+class CardController extends AbstractController
+{
+    #[Route('/{id}/card', name: 'app_client_card')]
+    public function index(Establishment $establishment): Response
+    {   
+        $card = $establishment->getCards();
+
+        return $this->render('card/index.html.twig', [
+            'card' => $card,
+            'controller_name' => 'CardController',
+        ]);
+    }
+}
