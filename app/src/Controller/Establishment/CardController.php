eb0ba1dd473ac3847df413afbe8b681a4d694c78
RaphaelPostal <raphael.postal@etudiant.univ-reims.fr>
2022-04-06 16:15:20 +0200

    edit Card and Establishment.php

--- app/src/Controller/Establishment/CardController.php
+++ app/src/Controller/Establishment/CardController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Establishment;
 
+use App\Entity\Section;
+use App\Form\SectionType;
 use App\Repository\CardRepository;
+use App\Repository\SectionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,13 +21,23 @@ class CardController extends AbstractController
      * @return Response
      */
     #[Route('', name: 'establishment_card_index')]
-    public function index(EntityManagerInterface $entityManager, CardRepository $cardRepository): Response
+    public function index(EntityManagerInterface $entityManager, CardRepository $cardRepository, SectionRepository $sectionRepository, Request $request): Response
     {
         //use when project is more advanced
         // $card = $this->getUser()->getEstablishment()->getCard();
 
         //find the first card in database
         $card = $cardRepository->find(1);
+
+        $section = new Section();
+        $form = $this->createForm(SectionType::class, $section);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $section->setCard($card);
+            $entityManager->persist($section);
+            $entityManager->flush();
+        }
 
         return $this->render('establishment/card/index.html.twig', [
             'card' => $card,
