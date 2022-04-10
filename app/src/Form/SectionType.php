fc4e995d9971c2ff7e67b86d225e1aa922e365ad
RaphaelPostal <raphael.postal@etudiant.univ-reims.fr>
2022-04-10 18:05:44 +0200

    keep displaying modal for multiple sections adding

--- app/src/Form/SectionType.php
+++ app/src/Form/SectionType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Section;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,22 @@ class SectionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'Nom',
+                //set a placeholder
+                'attr' => [
+                    'placeholder' => 'ex: Boissons',
+                ],
+            ])
+
+            ->add('save', SubmitType::class, [
+                'label' => 'Ajouter',
+                'attr' => [
+                    'id' => 'btn-section-save',
+                    'class' => 'btn btn-primary btn-section-save',
+                ],
+            ])
         ;
     }
 
