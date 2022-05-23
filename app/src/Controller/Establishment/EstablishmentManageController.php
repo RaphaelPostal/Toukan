c512127800da9d667bc754caf5075579ad5cba7b
RaphaelPostal <72035795+RaphaelPostal@users.noreply.github.com>
2022-05-23 19:59:40 +0200

    feat : edit information

--- app/src/Controller/Establishment/EstablishmentManageController.php
+++ app/src/Controller/Establishment/EstablishmentManageController.php
@@ -4,9 +4,14 @@ namespace App\Controller\Establishment;
 
 use App\Entity\Establishment;
 use App\Entity\User;
+use App\Form\EstablishmentInfoType;
+use App\Repository\EstablishmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Turbo\TurboBundle;
 
 class EstablishmentManageController extends AbstractController
 {
@@ -22,5 +27,43 @@ class EstablishmentManageController extends AbstractController
             'user' => $user,
             'establishment' => $establishment,
         ]);
+    }
+
+    #[Route('/establishment/information-edit', name: 'app_establishment_information_edit', methods: ['GET', 'POST'])]
+    public function editInformation(Request $request, EstablishmentRepository $establishmentRepository, EntityManagerInterface $entityManager): Response
+    {
+//        $establishment = $this->getUser()->getEstablishment();
+//        $user = $this->getUser();
+
+        $user = $this->getDoctrine()->getRepository(User::class)->find(1);
+        $establishment = $establishmentRepository->find(1);
+        $form = $this->createForm(EstablishmentInfoType::class, $establishment, [
+            'action' => $this->generateUrl('app_establishment_information_edit'),
+            'email' => $user->getEmail(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $email = $form->get('email')->getData();
+            $user->setEmail($email);
+            $establishmentRepository->add($establishment);
+
+            $entityManager->flush($user);
+
+            if (TurboBundle::STREAM_FORMAT === $request->getPreferredFormat()) {
+                // If the request comes from Turbo, set the content type as text/vnd.turbo-stream.html and only send the HTML to update
+                $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+                return $this->render('establishment/stream/information.stream.html.twig', ['establishment' => $establishment, 'user' => $user]);
+            }
+            return $this->redirectToRoute('app_establishment_manage', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('establishment/information/edit.html.twig', [
+            'establishment' => $establishment,
+            'form' => $form,
+        ]);
+
     }
 }
---
+++ app/src/Form/EstablishmentInfoType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Establishment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EstablishmentInfoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'mapped' => false,
+                'data' => $options['email']
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Establishment::class,
+            'email' => null
+        ]);
+    }
+}
